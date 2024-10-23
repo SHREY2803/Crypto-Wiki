78edf6c185eb69ec05631910c2e9c96ac86d0941
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-23 17:26:12 +0530

    BrowserRouter and routes defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './components/Header';
+import "./App.css";
+import HomePage from './Pages/HomePage.jsx';
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path='/' component={HomePage}/>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
