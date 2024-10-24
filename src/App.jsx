a485fb981799365bbf6d4e86ffa49ec6ecb2aaed
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-24 13:39:55 +0530

    Routes defined for mainPage and CoinsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Updated import
 import Header from './components/Header';
-import "./App.css";
+import './App.css';
 import HomePage from './Pages/HomePage.jsx';
+import { makeStyles } from '@mui/styles';
+import CoinsPage from './Pages/CoinsPage.jsx';
 
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color:"white",
+    minHeight: "100vh",
+  },
+}));
 function App() {
-  
+
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
-        <Route path='/' component={HomePage}/>
-
+        <Routes> {/* Wrap Route inside Routes */}
+          <Route path="/" element={<HomePage />} exact/> {/* Use element prop instead of component */}
+        </Routes>
+        <Routes> {/* Wrap Route inside Routes */}
+          <Route path="/coins" element={<CoinsPage />} /> {/* Use element prop instead of component */}
+        </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
