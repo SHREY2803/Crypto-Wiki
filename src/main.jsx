b091c7bd108142fc2c25a86ae4fcc3e30ae32d93
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-27 18:47:33 +0530

    Added alice-carousel.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import CryptoContext from './CryptoContext.jsx'
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
