f0ff234f7824a3334c5eae15c6eda168328fc665
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-27 20:01:53 +0530

    Updated all the coins in the carousel.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,43 +1,101 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@mui/styles";
 import axios from "axios";
-import { TrendingCoins } from '../config/api';
-import { CryptoState } from '../CryptoContext';
+import { TrendingCoins } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
 
+const useStyles = makeStyles(() => ({
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+  carouselItem: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    cursor: "pointer",
+    textTransform: "uppercase",
+    color: "white",
+  },
+}));
 
-
-const useStyles = makeStyles(()=>({
-    carousel:{
-        height:"50%",
-        display:"flex",
-        alignItems: "center"
-
-    },
-}))
+export function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+  
 
 const Carousel = () => {
-    const [trending,setTrending] = useState([]);
-    const classes = useStyles();    
+  const [trending, setTrending] = useState([]);
+  const classes = useStyles();
 
-    const { currency } = CryptoState();
-    const fetchCoins = async () => {
-        const { data } = await axios.get(TrendingCoins(currency));
+  const { currency, symbol } = CryptoState();
+  const fetchCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency));
 
-        setTrending(data);
-    }
-    console.log(trending);
-    // console.log(data);
-    
-    
+    setTrending(data);
+  };
+//   console.log(trending);
+  // console.log(data);
 
-    useEffect(()=>{
-        fetchCoins();
-    },[currency])
+  useEffect(() => {
+    fetchCoins();
+  }, [currency]);
+
+  const items = trending.map((coin) => {
+    let profit = coin?.price_change_percentage_24h >= 0;
+    return (
+      <Link to={`/coins/${coin.id}`} className={classes.carouselItem}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500,
+            }}
+          >
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    );
+  });
+
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
   return (
     <div className={classes.carousel}>
-        carousel 
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableButtonsControls
+        responsive={responsive}
+        autoPlay
+        items={items}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
