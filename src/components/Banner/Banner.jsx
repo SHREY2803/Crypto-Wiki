9c96e4d212fa1b09db26573bb75f507417de451a
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-27 17:24:05 +0530

    added the bg-img and content of it.

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,66 @@
+import { makeStyles } from "@mui/styles";
+import Container from "@mui/material/Container";
+import React from 'react';
+import Typography from "@mui/material/Typography";
+import CrypToImg from "../../assets/crypto_bg_img.jpg"; 
+import SecondImg from "../../assets/banner2.jpg"
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: `url(${SecondImg})`, 
+  },
+
+  bannerContent:{
+    height: 400, 
+    display: "flex",
+    flexDirection:"column",
+    paddingTop:25,
+    justifyContent:"space-around", 
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+        <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Rubik",
+            }}
+          >
+            Crypto Hunter
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Rubik",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
