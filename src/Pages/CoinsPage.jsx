480138b1ad3540cabf032a5d10e3471be83ce636
Shrey Kulkarni <shrey280302@gmail.com>
2024-11-10 23:47:14 +0530

    sidebar made

--- src/Pages/CoinsPage.jsx
+++ src/Pages/CoinsPage.jsx
@@ -4,6 +4,8 @@ import { CryptoState } from "../CryptoContext";
 import CoinChart from "../components/CoinChart";
 import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
+import { numberWithCommas } from "../components/Carousel";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const theme = createTheme();
 
@@ -17,16 +19,15 @@ const Container = styled("div")(({ theme }) => ({
 }));
 
 const Sidebar = styled("div")({
-  // Additional styles for the sidebar if needed
   width: "30%",
-  [theme.breakpoints.down("md")]: {
-    width: "100%",
-  },
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
   marginTop: 25,
   borderRight: "2px solid grey",
+  [theme.breakpoints.down("md")]: {
+    width: "100%",
+  },
 });
 
 const CoinName = styled("div")({
@@ -63,34 +64,126 @@ const CoinsPage = () => {
 
   useEffect(() => {
     fetchCoinData();
-  }, []);
+  }, [id]);
 
   // console.log(coins);
-  
+
+  if (!coins) return <LinearProgress style={{ backgroundColor: "gold" }} />;
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Sidebar>
-          <img
-            src={coins?.image.large}
-            alt={coins?.name}
-            style={{ marginBottom: 20 }}
-            height="200"
-          />
-        </Sidebar>
-        <Typography variant="h3">
-          <CoinName>{coins?.name}</CoinName>
-        </Typography>
-        <Typography variant="subtitle1">
-          {
-            coins?.description.en.split(". ")[0]
-          }.
-          
-        </Typography>
-        <CoinChart coin={coins} />
-      </Container>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Sidebar>
+            <img
+              src={coins?.image.large}
+              alt={coins?.name}
+              style={{ marginBottom: 20 }}
+              height="200"
+            />
+
+            <Typography variant="h3">
+              <CoinName>{coins?.name}</CoinName>
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              sx={{
+                width: "100%", // Ensure full width for small devices
+                fontFamily: "Rubik, sans-serif", // Font style
+                padding: "15px", // Padding for small screens
+                paddingTop: "10px", // Top padding
+                paddingBottom: "15px", // Bottom padding
+                textAlign: "justify", // Justify the text alignment
+                marginBottom: "20px", // Space between elements (for readability)
+                lineHeight: 1.6, // Improve line height for readability
+              }}
+              dangerouslySetInnerHTML={{
+                __html: coins?.description.en.split(". ")[0],
+              }}
+            />
+
+            <div
+              sx={{
+                alignSelf: "center",
+                padding: 15,
+                paddingTop: 10,
+                width: "100%",
+                [theme.breakpoints.down("md")]: {
+                  display: "flex",
+                  justifyContent: "space-around",
+                },
+                [theme.breakpoints.down("sm")]: {
+                  flexDirection: "column",
+                  alignItems: "center",
+                },
+                [theme.breakpoints.down("xs")]: {
+                  alignItems: "start",
+                },
+              }}
+            >
+              <span style={{ display: "flex" }}>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontWeight: "bold",
+                    marginBottom: 3,
+                    fontFamily: "Rubik",
+                  }}
+                >
+                  Rank:
+                </Typography>
+                &nbsp; &nbsp;
+                <Typography variant="h5" style={{ fontFamily: "Rubik" }}>
+                  {coins?.market_cap_rank}
+                </Typography>
+              </span>
+              <span style={{ display: "flex" }}>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontWeight: "bold",
+                    marginBottom: 3,
+                    fontFamily: "Rubik",
+                  }}
+                >
+                  Current Price:
+                </Typography>
+                &nbsp; &nbsp;
+                <Typography variant="h5" style={{ fontFamily: "Rubik" }}>
+                  {symbol}{" "}
+                  {numberWithCommas(
+                    coins?.market_data.current_price[currency.toLowerCase()]
+                  )}
+                </Typography>
+              </span>
+              <span style={{ display: "flex" }}>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontWeight: "bold",
+                    marginBottom: 3,
+                    fontFamily: "Rubik",
+                  }}
+                >
+                  Market Cap:
+                </Typography>
+                &nbsp; &nbsp;
+                <Typography variant="h5" style={{ fontFamily: "Rubik" }}>
+                  {symbol}{" "}
+                  {numberWithCommas(
+                    coins?.market_data.market_cap[currency.toLowerCase()]
+                      .toString()
+                      .slice(0, -6)
+                  )}{" "}
+                  M
+                </Typography>
+              </span>
+            </div>
+          </Sidebar>
+          <CoinChart coin={coins} />
+        </Container>
+      </ThemeProvider>
+    </>
   );
 };
 
