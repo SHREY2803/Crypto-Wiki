e91f37ab1dcdead73f42a39b261576fb109fff0d
Shrey Kulkarni <shrey280302@gmail.com>
2024-11-12 19:54:30 +0530

    this contains all my web app's Firebase configuration

---
+++ src/config/fireBaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLPQCOmy23wZuTGEekGJpEKI1Bbg5oIPQ",
+    authDomain: "mycryptowallet-c04b9.firebaseapp.com",
+    projectId: "mycryptowallet-c04b9",
+    storageBucket: "mycryptowallet-c04b9.firebasestorage.app",
+    messagingSenderId: "167579237213",
+    appId: "1:167579237213:web:e1c56b3ac762d050cf46b4"
+  };
+
+  export default firebaseConfig;
